5dd1f8d883233d253302c55370add8d092599ca7
LiuLongChang <1040719208@qq.com>
2018-03-30 18:07:55 +0800

    提交所绘制表格

--- BezierCurveLineDemo/BezierCurveView.swift
+++ BezierCurveLineDemo/BezierCurveView.swift
@@ -12,7 +12,9 @@ import UIKit
 let Margin : CGFloat = 30
 
 //y轴每一个值的间隔数
-let Y_Every_Margin : CGFloat = 20
+let Y_Every_Margin : CGFloat = 30
+
+let X_Every_Margin : CGFloat = 28
 
 
 //颜色RGB
@@ -43,7 +45,7 @@ class BezierCurveView: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         let backView = UIView.init(frame: CGRect.init(x: 0, y: 0, width: frame.size.width, height: frame.size.height))
-        backView.backgroundColor = RGBColor(255, 229, 239)
+        backView.backgroundColor = RGBColor(255, 255, 255)
         self.addSubview(backView)
         BezierCurveView.myFrame = frame
     }
@@ -54,7 +56,6 @@ class BezierCurveView: UIView {
         //1. Y轴、X轴的直线
         path.move(to: CGPoint.init(x: Margin, y: BezierCurveView.myFrame.height-Margin))
         path.addLine(to: CGPoint.init(x: Margin, y: Margin))
-
 
         path.move(to: CGPoint.init(x: Margin, y: BezierCurveView.myFrame.height-Margin))
         path.addLine(to: CGPoint.init(x: Margin+BezierCurveView.myFrame.width-2*Margin, y: BezierCurveView.myFrame.height-Margin))
@@ -68,7 +69,8 @@ class BezierCurveView: UIView {
         }
 
         //Y轴 实际长度为200，此处比例缩小一倍使用
-        for idx in 0..<11{
+        let yArray = [0,2,4,6,8,10,12,14]
+        for idx in 0..<yArray.count{
             let y = BezierCurveView.myFrame.height - Margin - Y_Every_Margin*CGFloat(idx)
             let point = CGPoint.init(x: Margin, y: y)
             path.move(to: point)
@@ -77,30 +79,32 @@ class BezierCurveView: UIView {
 
         //添加索引格文字 X轴
         for idx in 0..<x_names.count {
-            let x = Margin+15+Margin*CGFloat(idx)
+            let x = Margin+15+X_Every_Margin*CGFloat(idx)
             let textLabel = UILabel(frame: CGRect.init(x:x, y: BezierCurveView.myFrame.height-Margin, width: Margin, height: 20))
+            if idx % 2 == 1 {textLabel.isHidden = true}
+            textLabel.textAlignment = .center
             textLabel.text = x_names[idx]
-            textLabel.font = UIFont.systemFont(ofSize: 10)
-            textLabel.textColor = UIColor.blue
+            textLabel.font = UIFont.systemFont(ofSize: 12)
+            textLabel.textColor = UIColor.init(red: 51/255.0, green: 51/255.0, blue: 51/255.0, alpha: 1)
             self.addSubview(textLabel)
         }
 
         //添加索引格文字  Y轴
-        for idx in 0..<11{
+        for (idx,obj) in yArray.enumerated(){
             let y = BezierCurveView.myFrame.height - Margin - Y_Every_Margin*CGFloat(idx);
             let textLabel = UILabel.init(frame: CGRect.init(x: 0, y: y-5, width: Margin, height: 10))
-            textLabel.text = "\(10*idx)"
-            textLabel.font = UIFont.systemFont(ofSize: 10)
+            textLabel.text = "\(obj)k";
+            textLabel.font = UIFont.systemFont(ofSize: 12)
             textLabel.textAlignment = .center
-            textLabel.textColor = UIColor.red
+            textLabel.textColor = UIColor.init(red: 51/255.0, green: 51/255.0, blue: 51/255.0, alpha: 1)
             self.addSubview(textLabel)
         }
 
         //渲染路径
         let shapeLayer = CAShapeLayer()
         shapeLayer.path = path.cgPath
-        shapeLayer.strokeColor = UIColor.black.cgColor
-        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = UIColor.init(red: 230/255.0, green: 235/255.0, blue: 245/255.0, alpha: 1).cgColor
+        shapeLayer.fillColor = UIColor.init(red: 230/255.0, green: 235/255.0, blue: 245/255.0, alpha: 1).cgColor
         shapeLayer.borderWidth = 2.0
         self.subviews[0].layer.addSublayer(shapeLayer)
     }
@@ -117,7 +121,13 @@ class BezierCurveView: UIView {
             let x = Margin+Margin*CGFloat(idx+1)
             let y = BezierCurveView.myFrame.height-Margin-CGFloat(doubleValue)
             let point = CGPoint.init(x: x, y: y)
-            let path = UIBezierPath.init(roundedRect: CGRect.init(x: point.x-1, y: point.y-1, width: 2.5, height: 2.5), cornerRadius: 2.5)
+            var path : UIBezierPath! = nil;
+            if idx % 2 == 1 {
+                path = UIBezierPath.init(roundedRect: CGRect.init(x: point.x-1, y: point.y-1, width: 0, height: 0), cornerRadius: 2.5)
+            }else{
+                path = UIBezierPath.init(roundedRect: CGRect.init(x: point.x-1, y: point.y-1, width: 2.5, height: 2.5), cornerRadius: 2.5)
+            }
+
             let layer = CAShapeLayer()
             layer.strokeColor = UIColor.purple.cgColor
             layer.fillColor = UIColor.purple.cgColor
@@ -162,6 +172,14 @@ class BezierCurveView: UIView {
             label.textAlignment = .center
             label.font = UIFont.systemFont(ofSize: 10)
             self.subviews[0].addSubview(label)
+
+            if idx % 2 == 0 {
+                label.isHidden = false
+            }else{
+                label.isHidden = true
+            }
+
+
             if idx == 0{
                 let nowPoint = allPoints[0]
                 label.text = "\((BezierCurveView.myFrame.height-(nowPoint as! CGPoint).y-Margin)/2.0)"
--- BezierCurveLineDemo/ViewController.swift
+++ BezierCurveLineDemo/ViewController.swift
@@ -16,10 +16,13 @@ class ViewController: UIViewController {
         
         let kScreenWidth = UIScreen.main.bounds.size.width;
         
-        let x_names = ["语文","数学","英语","物理","化学","生物","政治","历史","地理"];
-        let targets = ["20","40","20","50","30","90","30","100","70"];
+        let x_names = ["1年","2年","3年","4年","5年","6年","7年","8年","9年"];
+
+        let targets = ["2","4","6","8","10","12","14"];
+
+
         
-        let bezierView = BezierCurveView.init(frame: CGRect.init(x: 30, y: 30, width: kScreenWidth-60.0, height: 280))
+        let bezierView = BezierCurveView.init(frame: CGRect.init(x: 30, y: 30, width: kScreenWidth-60.0, height: 240+56))
         bezierView.center = self.view.center
         self.view.addSubview(bezierView)
         
